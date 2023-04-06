cb109c7e0885407d65587b83f0245d052b6cb87e
dg1223 <alavi1223@hotmail.com>
2023-04-06 18:25:34 -0400

    remove items from cart, toast

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,25 @@ import { useLoaderData, useSearchParams } from "react-router-dom";
 import TShirt from "../TShirt/TShirt";
 import Cart from "../Cart/Cart";
 import "./Home.css";
+import { toast } from "react-hot-toast";
 
 const Home = () => {
   const tshirts = useLoaderData();
   const [cart, setCart] = useState([]);
 
   const handleAddToCart = (tshirt) => {
-    console.log(tshirt);
+    const exists = cart.find((ts) => ts._id === tshirt._id);
+    if (exists) {
+      toast("You have already added this TShirt");
+    } else {
+      const newCart = [...cart, tshirt];
+      setCart(newCart);
+    }
+  };
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((tshirt) => tshirt._id !== id);
+    setCart(remaining);
   };
 
   return (
@@ -24,7 +36,7 @@ const Home = () => {
         ))}
       </div>
       <div className="cart-container">
-        <Cart></Cart>
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
       </div>
     </div>
   );
